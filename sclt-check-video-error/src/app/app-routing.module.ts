48571e1a20e615cf8ac5aa94ee8934ebaabf0ff5
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2020-08-12 15:44:43 +0700

    Revert "Accessing  param by fragments"
    This reverts commit fb24a6bebcef026862120c714be34b50fe3bf065.

--- sclt-check-video-error/src/app/app-routing.module.ts
+++ sclt-check-video-error/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  { path: 'check-data', loadChildren:() => import('./check-data/check-data.module').then(m => m.CheckDataModule)},
-
-  { path: '', component:PageNotFoundComponent },
-  { path: '**', component:PageNotFoundComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sclt-check-video-error/src/app/check-data/check-video/check-video.component.ts
+++ sclt-check-video-error/src/app/check-data/check-video/check-video.component.ts
@@ -1,32 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Router, ActivatedRoute, ParamMap} from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-
 @Component({
   selector: 'app-check-video',
   templateUrl: './check-video.component.html',
   styleUrls: ['./check-video.component.css']
 })
-export class CheckVideoComponent implements OnInit {  
-  route : ActivatedRoute;
-  url : String;
-  url$ : Observable<String>;
+export class CheckVideoComponent implements OnInit {
 
-  constructor(route : ActivatedRoute) {    
-    this.route = route;    
-  }
+  constructor() { }
 
-  ngOnInit(){
-    this.route.paramMap.subscribe(param =>{
-      this.url = param.get("url");
-    });     
-    
-  }
-
-  goToCheck(url: String){
-
+  ngOnInit(): void {
   }
 
 }
