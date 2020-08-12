9d87314307477a70c7a431901a98e82e6563953b
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2020-08-12 15:46:41 +0700

    Revert "Revert "Accessing  param by fragments""
    This reverts commit 48571e1a20e615cf8ac5aa94ee8934ebaabf0ff5.

--- sclt-check-video-error/src/app/app-routing.module.ts
+++ sclt-check-video-error/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'check-data', loadChildren:() => import('./check-data/check-data.module').then(m => m.CheckDataModule)},
+
+  { path: '', component:PageNotFoundComponent },
+  { path: '**', component:PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
