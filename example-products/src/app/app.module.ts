2f1c6aeed5caae8becbb3f1b5019af84c8537819
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2020-08-14 09:50:07 +0700

    Use forms for user input
    Add form to cart view

--- example-products/src/app/app.module.ts
+++ example-products/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
@@ -24,6 +25,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     BrowserModule,    
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule ,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- example-products/src/app/cart/cart.component.ts
+++ example-products/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +9,26 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkOutForm;
 
   constructor(
-    private cartService : CartService
-  ) { }
+    private cartService : CartService,
+    private formBuilder : FormBuilder,
+  ) { 
+    this.checkOutForm = formBuilder.group({
+      name : '',
+      address : '',
+    });
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData){
+    this.cartService.clearCart();
+    this.checkOutForm.reset();
+
+    console.warn("Your order has been submited!",customerData);
+  }
 }
