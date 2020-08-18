951431b020ff612b5951ea67faa8ddd360bb129f
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2020-08-18 14:25:34 +0700

    Heroes Tour: 6-Get data from server: get hero by id

--- heroes-tour/src/app/hero.service.ts
+++ heroes-tour/src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './moc-heroes';
@@ -11,7 +12,7 @@ import { MessagesService } from './messages.service';
   providedIn: 'root'
 })
 export class HeroService {
-  private heroesUrl ="/app/heroes";
+  private heroesUrl ="/api/heroes";
   constructor(
     private messageService :MessagesService,
     private http : HttpClient
@@ -22,15 +23,35 @@ export class HeroService {
   //   return of(HEROES);
   // }
   getHeroes() : Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap( _ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes',[]))
+    );
   }
 
-  getHero(id : number) : Observable<Hero> {
-    this.messageService.add(`HeroesComponent: Selected hero id=${id}`);
-    return of(HEROES.find(hero => hero.id===id));
+  // getHero(id : number) : Observable<Hero> {
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${id}`);
+  //   return of(HEROES.find(hero => hero.id===id));
+  // }
+  getHero(id : number) : Observable<Hero>{
+    const url = '${this.heroesUrl}/${id}';
+    return this.http.get<Hero>(url).pipe(
+      tap(( _ =>this.log('fetcheed hero id=${id}'))),
+      catchError(this.handleError<Hero>('getHero id=${id}'))
+    );
   }
   
   private log(message : string){
-    this.messageService.add(message);
+    this.messageService.add("Heroes Service:{$message}");
+  }
+
+  private handleError <T>(operation = 'operation', result?:T){
+    return (error : any) : Observable<T> => {
+      //log error to console and complie
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      
+      return of(result as T);
+    }
   }
 }
